d7c99d437baa6d920ddd18635ca6ed3e1a323c23
fitman <himanshuverma2660@gmail.com>
2026-08-11 16:21:46 +0530

    Finish implementation of eslint.config.mjs

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -10,3 +10,9 @@ const eslintConfig = defineConfig([
     // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+]);
+
+export default eslintConfig;
