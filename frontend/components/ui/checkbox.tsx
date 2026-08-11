88b1924548146849d9438952f5c2cca3b785a991
fitman <himanshuverma2660@gmail.com>
2026-08-11 16:21:44 +0530

    Start implementation of checkbox.tsx

---
+++ frontend/components/ui/checkbox.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+import { Checkbox as CheckboxPrimitive } from "@base-ui/react/checkbox"
+
+import { cn } from "@/lib/utils"
+import { CheckIcon } from "lucide-react"
+
+function Checkbox({ className, ...props }: CheckboxPrimitive.Root.Props) {
+  return (
+    <CheckboxPrimitive.Root
