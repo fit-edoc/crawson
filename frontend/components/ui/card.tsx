16acaf4a8cffb226ba3862561cfdc1478f272b1d
fitman <himanshuverma2660@gmail.com>
2026-08-11 16:21:43 +0530

    Continue implementation of card.tsx

--- frontend/components/ui/card.tsx
+++ frontend/components/ui/card.tsx
@@ -33,3 +33,38 @@ function CardHeader({ className, ...props }: React.ComponentProps<"div">) {
   )
 }
 
+function CardTitle({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="card-title"
+      className={cn(
+        "font-heading text-base leading-snug font-medium group-data-[size=sm]/card:text-sm",
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
+function CardDescription({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="card-description"
+      className={cn("text-sm text-muted-foreground", className)}
+      {...props}
+    />
+  )
+}
+
+function CardAction({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="card-action"
+      className={cn(
+        "col-start-2 row-span-2 row-start-1 self-start justify-self-end",
+        className
+      )}
+      {...props}
+    />
+  )
+}
