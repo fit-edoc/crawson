eb617d2015500d053016faa33aa9225a0d7572ff
fitman <himanshuverma2660@gmail.com>
2026-08-11 16:21:45 +0530

    Start implementation of switch.tsx

---
+++ frontend/components/ui/switch.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import { Switch as SwitchPrimitive } from "@base-ui/react/switch"
+
+import { cn } from "@/lib/utils"
+
+function Switch({
+  className,
+  size = "default",
+  ...props
+}: SwitchPrimitive.Root.Props & {
